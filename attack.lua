c97da260cc01365290fa4536e480065ba8fec29c
giahuy908 <gle158603@gmail.com>
2026-06-09 20:36:31 +0700

    Add files via upload

---
+++ attack.lua
@@ -0,0 +1,5 @@
+-- HuyDepZai Fast attack
+loadstring(game:HttpGet("https://raw.githubusercontent.com/giahuy908/Blox-Fruit/refs/heads/main/fastattack.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/giahuy908/Blox-Fruit/refs/heads/main/auto.txt"))()
+loadstring(game:httpget("https://raw.githubusercontent.com/anhdzaiscript/setting/refs/heads/main/fastmax.lua"))()
+loadstring(game:HttpGet("https://pastebin.com/raw/fGL2kZMT"))()
